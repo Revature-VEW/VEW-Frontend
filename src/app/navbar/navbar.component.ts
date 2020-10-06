c674a5945cd477eb5ff5a37a53b59959eef216f9
Eurus Wagner <euruswagner@gmail.com>
2020-10-05 20:41:17 -0400

    added navbar

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  userId: number;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  isLoggedIn(): boolean {
+    if (JSON.parse(localStorage.getItem('userInfo')) != null) {
+      this.userId = JSON.parse(localStorage.getItem('userInfo')).userId;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  logout(): void {
+    localStorage.setItem('userInfo', null);
+  }
+}
